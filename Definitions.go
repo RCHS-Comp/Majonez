5570a21d7cda4dc3ab4e47a6c01c777a7a427fa5
Rhea County High School <63121534+RCHS-Comp@users.noreply.github.com>
2021-02-18 17:10:16 -0600

    Provisioning for Version 1.1.0 (February 18, 2021) (17:07)
    "So before, the version of HGF was 1.0.0 but not explicitly stated. I fixed that this time so we can have different versions of HGF and Majonez"

---
+++ Definitions.go
@@ -0,0 +1,8 @@
+package main
+
+// HGF version 1.1.0
+// (c) Harry Nelsen 2021
+
+var echo = Meta{Name: "echo", Arguments: 1, InputTypes: []int{1}, OutputType: 0}
+var rem = Meta{Name: "rem", Arguments:1, InputTypes: []int{7}, OutputType: 0}
+var test = Meta{Name: "test", Arguments: 0, InputTypes: nil, OutputType: 0}
