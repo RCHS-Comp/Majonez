fc5d6720a06390eb09a5d95c1998cda1570d8988
Rhea County High School <63121534+RCHS-Comp@users.noreply.github.com>
2021-02-18 19:30:26 -0600

    Provisioning for Version 1.1.0 (February 18, 2021) (19:29)
    "*ALL* the commits today were messed up, they were meant to be for February 18 instead of February 11"

--- Definitions.go
+++ Definitions.go
@@ -4,5 +4,5 @@ package main
 // (c) Harry Nelsen 2021
 
 var echo = Meta{Name: "echo", Arguments: 1, InputTypes: []int{1}, OutputType: 0}
-var rem = Meta{Name: "rem", Arguments: 1, InputTypes: []int{7}, OutputType: 0}
-var test = Meta{Name: "test", Arguments: 0, InputTypes: nil, OutputType: 0}
+var rem  = Meta{Name: "rem",  Arguments: 1, InputTypes: []int{7}, OutputType: 0}
+var test = Meta{Name: "test", Arguments: 0, InputTypes: nil,      OutputType: 0}
