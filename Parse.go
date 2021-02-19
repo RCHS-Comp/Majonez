6aa93c90e407e065c0a9c1e2bf450e4fcbf07dbb
Rhea County High School <63121534+RCHS-Comp@users.noreply.github.com>
2021-02-18 19:37:43 -0600

    Provisioning for Version 1.1.0 (February 18, 2021) (19:36)
    "I think this is the only commit today that would have the correct date"

---
+++ Parse.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+    "fmt"
+)
+
+var AllowedErrors = -1
+var Coloured = true
+
+// Due to an oversight (mainly me neglecting how we'd pipe commands) piping won't be added
+
+func P_I(Count int) (string) {
+    Output := ""
+    
+    for i := 0; i < Count; i ++ {
+        Output = Output + "    "
+    }
+    
+    return Output
+}
+
+func P_PrintError(Input string, Line int) {
+    ErrorSymbol := "[!]"
+
+    if Coloured {
+        ErrorSymbol = "\033[1;31m[!]\033[0m"
+    }
+    
+    fmt.Printf("%s Line %d: %s\n", ErrorSymbol, (Line + 1), Input)
+}
+
+func P_ParseFile(File []string) (int) {
+    var GlobalStr []string
+    var Output string
+    Errors := 0
+    VarOut := 0
+    
+    if !(CV_Setup()) {
+        P_PrintError("Couldn't setup correctly.", -1)
+        Errors ++
+    }
+
+    for CurrentLine, FileLine := range File {
+        LineCommands, GlobalStr := CMD_ReadLine(FileLine, GlobalStr)
+        
+        for _, Command := range LineCommands {
+            VarOut, Output = CV_Convert(Command, GlobalStr)
+            if VarOut == 5 {
+                Errors ++
+                P_PrintError(Output, CurrentLine)
+            }
+            
+            if (Errors > AllowedErrors && !(AllowedErrors == -1)) {
+                return Errors
+            }
+        }
+    }
+    
+    if !(CV_Teardown()) {
+        P_PrintError("Couldn't teardown correctly.", len(File))
+        Errors ++
+    }
+
+    return Errors
+}
