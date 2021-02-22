5a81cd0f909a1e5957a38bda621382b8883a0fe7
Rhea County High School <63121534+RCHS-Comp@users.noreply.github.com>
2021-02-21 21:37:28 -0600

    Version 1.1.0 (February 21, 2021) (21:36)
    "Add more things to make life better."

--- Majonez.go
+++ Majonez.go
@@ -7,7 +7,43 @@ import (
     "strings"
     "os"
     "bufio"
+    "runtime"
 )
+
+var MJ_V_Major = 1
+var MJ_V_Minor = 1
+var MJ_V_Micro = 0
+var MJ_Year = 2021
+
+var MJ_InfoTriggers = []string{"-h", "--help", "--info", "--about"}
+
+func MJ_Info() {
+    fmt.Println("//////////////////////////////////////////")
+    fmt.Printf("Majonez [Version %d.%d.%d]\n", MJ_V_Major, MJ_V_Minor, MJ_V_Micro)
+    fmt.Printf("OS: %s, Arch: %s\n", runtime.GOOS, runtime.GOARCH)
+    fmt.Printf("(c) Harry Nelsen %d\n", MJ_Year)
+    fmt.Println("//////////////////////////////////////////")
+    
+    fmt.Println("Settings:")
+    if AllowedErrors == -1 {
+        fmt.Println("Errors allowed: Unlimited")
+    } else {
+        fmt.Printf("Errors allowed: %d\n", AllowedErrors)
+    }
+    fmt.Printf("HGF Version: %d.%d.%d\n", HGF_V_Major, HGF_V_Minor, HGF_V_Micro)
+    fmt.Println("//////////////////////////////////////////")
+    
+    fmt.Println("Unavailable variables:")
+    for _, VarName := range VariableUnavailable {
+        fmt.Println(VarName)
+    }
+    fmt.Println("//////////////////////////////////////////")
+    
+    fmt.Println("Usage:")
+    fmt.Println("Majonez FILENAME   -   Convert FILENAME")
+    fmt.Println("Majonez --about    -   Show this menu")
+    fmt.Println("//////////////////////////////////////////")
+}
 
 func MJ_WriteFile(ToWrite string) {
     // fmt.Println(ToWrite)
@@ -51,11 +87,20 @@ func MJ_ReadFile(FileName string) ([]string) {
 }
 
 func main() {
-    var Errors int
+    Errors := 0
 
     if len(os.Args) > 1 {
-        Errors = P_ParseFile(MJ_ReadFile(os.Args[1]))
+        CMD_Arg := os.Args[1]
+    
+        if In_string(MJ_InfoTriggers, CMD_Arg) == -1 {
+            fmt.Printf("Reading %s...\n", CMD_Arg)
+            Errors = P_ParseFile(MJ_ReadFile(CMD_Arg))
+        } else {
+            MJ_Info()
+        }
+    } else {
+        fmt.Println("No arguments.")
     }
     
-    fmt.Printf("Finished with %d error(s)\n", Errors)
+    fmt.Printf("\nFinished with %d error(s)\n", Errors)
 }
