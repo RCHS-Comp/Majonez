862476e8d93cb606e79f6573c99cf17fbcfe586d
Rhea County High School <63121534+RCHS-Comp@users.noreply.github.com>
2021-04-01 21:20:01 -0500

    Update types
    This will probably be rewritten in the future

--- extensions/Python.go
+++ extensions/Python.go
@@ -25,19 +25,19 @@ func CV_Convert(Input Command, GlobalStr []string) (int, string) {
     switch Input.Name {
         case "echo":
             if !(echo.Compliant(Input)) {
-                return 5, "Invalid type. (Only strings supported.)"
+                return 41, "Invalid type. (Only strings supported.)"
             }
             
             Output = fmt.Sprintf("print(%s)", Input.Arguments[0])
         case "test":
             if !(test.Compliant(Input)) {
-                return 5, "Invalid usage."
+                return 41, "Invalid usage."
             }
             
             Output = "# Success"
         case "rem":
             if !(rem.Compliant(Input)) {
-                return 5, "Invalid usage."
+                return 41, "Invalid usage."
             }
             
             Output = "# " + Input.Arguments[0]
@@ -48,83 +48,83 @@ func CV_Convert(Input Command, GlobalStr []string) (int, string) {
         
         case "if":
             if !(if_statement.Compliant(Input)) {
-                return 5, "Invalid type."
+                return 41, "Invalid type."
             }
             
             Success := false    // Create here because if we do "Output, Success := ..." it will make Output unable to be returned
             Output, Success = IF_ParseStatement(Input, &IndentCount)
             
             if !(Success) {
-                return 5, Output
+                return 41, Output
             }
         case "elseif":
             if !(elseif_statement.Compliant(Input)) {
-                return 5, "Invalid type."
+                return 41, "Invalid type."
             }
             
             Success := false
             Output, Success = IF_ParseStatement(Input, &IndentCount)
             
             if !(Success) {
-                return 5, Output
+                return 41, Output
             }
         case "else":
             if !(else_statement.Compliant(Input)) {
-                return 5, "Invalid usage."
+                return 41, "Invalid usage."
             }
             
             Success := false
             Output, Success = IF_ParseStatement(Input, &IndentCount)
             
             if !(Success) {
-                return 5, Output
+                return 41, Output
             }
         case "endif":
             if !(endif_statement.Compliant(Input)) {
-                return 5, "Invalid usage."
+                return 41, "Invalid usage."
             }
             
             Success := false
             Output, Success = IF_ParseStatement(Input, &IndentCount)
             
             if !(Success) {
-                return 5, Output
+                return 41, Output
             }
         case "loop_st":
             if !(loop_start.Compliant(Input)) {
-                return 5, "Invalid usage."
+                return 41, "Invalid usage."
             }
             
             Success := false
             Output, Success = LOOP_ParseStatement(Input, &IndentCount)
             
             if !(Success) {
-                return 5, Output
+                return 41, Output
             }
         case "loop_nd":
             if !(loop_end.Compliant(Input)) {
-                return 5, "Invalid usage."
+                return 41, "Invalid usage."
             }
             
             Success := false
             Output, Success = LOOP_ParseStatement(Input, &IndentCount)
             
             if !(Success) {
-                return 5, Output
+                return 41, Output
             }
         case "loop_bk":
             if !(loop_break.Compliant(Input)) {
-                return 5, "Invalid usage."
+                return 41, "Invalid usage."
             }
             
             Success := false
             Output, Success = LOOP_ParseStatement(Input, &IndentCount)
             
             if !(Success) {
-                return 5, Output
+                return 41, Output
             }
         default:
-            return 5, "Missing command."
+            return 41, "Missing command."
     }
     
     if (In_string(IF_PossibleStatements, Input.Name) == -1 && In_string(LOOP_PossibleStatements, Input.Name) == -1) {
@@ -137,5 +137,5 @@ func CV_Convert(Input Command, GlobalStr []string) (int, string) {
         fmt.Println(Output)
     }
     
-    return 0, Output
+    return 99, Output
 }
