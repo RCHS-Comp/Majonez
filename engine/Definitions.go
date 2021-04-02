e3e2c277e06a24499f42131c673f035fe63675d7
Rhea County High School <63121534+RCHS-Comp@users.noreply.github.com>
2021-04-01 21:25:05 -0500

    Update types for definitions

--- engine/Definitions.go
+++ engine/Definitions.go
@@ -7,6 +7,6 @@ var HGF_V_Micro = 0
 // HGF version 1.1.0
 // (c) Harry Nelsen 2021
 
-var echo = Meta{Name: "echo", Arguments: 1, InputTypes: []int{1}, OutputType: 0}
-var rem  = Meta{Name: "rem",  Arguments: 1, InputTypes: []int{7}, OutputType: 0}
-var test = Meta{Name: "test", Arguments: 0, InputTypes: nil,      OutputType: 0}
+var echo = Meta{Name: "echo", Arguments: 1, InputTypes: []int{20},  OutputType: 20}
+var rem  = Meta{Name: "rem",  Arguments: 1, InputTypes: []int{40},  OutputType: 0}
+var test = Meta{Name: "test", Arguments: 0, InputTypes: nil,        OutputType: 20}
